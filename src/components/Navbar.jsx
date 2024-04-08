6f123fecad800d5279661fb71655aebca8648209
Rafi Ferdos <rafiferdos@gmail.com>
2024-04-08 14:53:20 +0600

    update active route style in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,12 @@ import {IoPersonOutline} from "react-icons/io5";
 const Navbar = () => {
     const links =
         <>
-            <NavLink to='/' className='hover:text-purple-700'><a>Home</a></NavLink>
-            <NavLink to='features' className='hover:text-purple-700'><a>Features</a></NavLink>
-            <NavLink to='contact_us' className='hover:text-purple-700'><a>Contact Us</a></NavLink>
-            <NavLink to='update_profile' className='hover:text-purple-700'><a>Update Profile</a></NavLink>
+            <NavLink to='/' className={({isActive}) => isActive ? "text-purple-700" :"hover:text-purple-400"}><a>Home</a></NavLink>
+            <NavLink to='features' className={({isActive}) => isActive ? "text-purple-700" :"hover:text-purple-400"}><a>Features</a></NavLink>
+            <NavLink to='contact_us' className={({isActive}) => isActive ? "text-purple-700" :"hover:text-purple-400"}><a>Contact Us</a></NavLink>
+            <NavLink to='update_profile' className={({isActive}) => isActive ? "text-purple-700" :"hover:text-purple-400"}><a>Update Profile</a></NavLink>
         </>
+    // hover:text-purple-700
     return (
         <>
             <div className="navbar bg-base-100 max-w-7xl mx-auto lg:py-6 w-11/12">
@@ -23,14 +24,14 @@ const Navbar = () => {
                             </svg>
                         </div>
                         <ul tabIndex={0}
-                            className="menu menu-sm dropdown-content mt-3 z-[1] shadow bg-base-100 rounded-box w-52 gap-4 p-4">
+                            className="menu menu-sm dropdown-content mt-3 z-[1] shadow bg-base-100 rounded-box w-52 gap-4 p-4 *:font-semibold">
                             {links}
                         </ul>
                     </div>
                     <a className="btn btn-ghost text-lg lg:text-2xl md:text-xl bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text animate-gradient bg-300%">Neko&apos;s Island</a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1 *:lg:text-lg *:opacity-70 gap-6">
+                    <ul className="menu menu-horizontal px-1 *:lg:text-lg *:opacity-70 gap-6 *:font-semibold">
                         {links}
                     </ul>
                 </div>
